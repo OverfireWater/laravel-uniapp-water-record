ad45e5709be0eddac1449f9664d08c3d1c584d9d
OverfireWater <2632686733@qq.com>
2024-02-27 16:05:29 +0800

    2024/2/27 16:05

--- Water_record_laravel/app/Http/Controllers/GongXueYunSignServer/GongXueYunSignController.php
+++ Water_record_laravel/app/Http/Controllers/GongXueYunSignServer/GongXueYunSignController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Mail;
 class GongXueYunSignController extends Controller
 {
     use CodeMsgDataTrait;
+
     public function sendMail(Request $request)
     {
         $email = $request->input('email');
@@ -17,19 +18,21 @@ class GongXueYunSignController extends Controller
         $code = $request->input('code');
         $Cache_email = 'GongXueYunSign' . $email;
         $now_date = date("Y-m-d H:i:s");
-        if ($code == 200){
+        if ($code == 200) {
             $text = "您好，本次工学云打卡 成功, 打卡时间为$now_date";
-        }else{
+        } else if ($code == 202) {
+            $text = "$msg, 时间为：$now_date";
+        } else {
             $text = "您好，本次工学云打卡 失败，失败原因：$msg, 打卡时间为$now_date";
         }
         try {
-            $res = Mail::raw($text, function ($message) use($email) {
+            $res = Mail::raw($text, function ($message) use ($email) {
                 $message->To($email);
                 $message->subject('工学云自助打卡');
             });
         } catch (\Exception $e) {
             $err_code = $e->getCode();
-            if ($err_code){
+            if ($err_code) {
                 $this->code = 551;
                 $this->msg = "发送失败";
             }
